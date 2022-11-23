652db52277908a29ecc0bf6348658ac77234e5dd
Paul <118658876+paul4training@users.noreply.github.com>
2022-11-23 18:49:33 +0200

    Update streamlit_app.py
    Fix missing closing quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+ "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json())
 
 # # take the json version of the response and normalize it 
