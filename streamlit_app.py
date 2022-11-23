782d26ab4da515d0a84021e4f0f84d6863d7c126
Paul <118658876+paul4training@users.noreply.github.com>
2022-11-23 20:39:29 +0200

    Update streamlit_app.py
    write test to snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ my_data_row = my_cur.fetchall()
 streamlit.header("Fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('Thank you for add ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
+streamlit.write('Thank you for adding ', add_my_fruit)
+my cur.execute("insert into fruit_load_list values ('from streamlit');")
 
